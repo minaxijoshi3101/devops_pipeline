8e3cf0ae624f69a499fc21fec9ae3aaa608873eb
minaxi-esamyak <minaxijoshi3101@gmail.com>
2026-02-22 20:31:56 +0800

    build groovy updated

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,39 +1,41 @@
-pipeline{
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                script {
-                    sh '''
-                        echo "Build stage"
-                    '''
+def call() {
+    pipeline{
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        sh '''
+                            echo "Build stage"
+                        '''
+                    }
                 }
             }
-        }
-        stage('Run Unit Tests') {
-            steps {
-                script {
-                    sh '''
-                        echo "Running unit tests"
-                    '''
+            stage('Run Unit Tests') {
+                steps {
+                    script {
+                        sh '''
+                            echo "Running unit tests"
+                        '''
+                    }
                 }
             }
-        }
-        stage('SonarQube Code Scan') {
-            steps {
-                script {
-                    sh '''
-                        echo "Running SonarQube code scan"
-                    '''
+            stage('SonarQube Code Scan') {
+                steps {
+                    script {
+                        sh '''
+                            echo "Running SonarQube code scan"
+                        '''
+                    }
                 }
             }
-        }
-        stage('Publish to Artifactory') {
-            steps {
-                script {
-                    sh '''
-                        echo "Publishing artifact to Artifactory"
-                    '''
+            stage('Publish to Artifactory') {
+                steps {
+                    script {
+                        sh '''
+                            echo "Publishing artifact to Artifactory"
+                        '''
+                    }
                 }
             }
         }
